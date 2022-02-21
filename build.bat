1ac4c4bb75d8b672af15add3fe0adf22e9a51679
Flutterish <CharmindYT@gmail.com>
2022-02-21 20:33:41 +0100

    use esbuild to bundle and minify the web scripts

--- build.bat
+++ build.bat
@@ -4,7 +4,8 @@ cd ../WebWorkers
 call tsc
 call tsc --p tsconfig.build.json
 cd ../Files
-call tsc
+call tsc -noEmit
+call ./../../node_modules/.bin/esbuild Scripts.ts --bundle --minify --outfile=./../../Files/Scripts.js
 cd ../..
 call node cleanup.js
 cd Build
